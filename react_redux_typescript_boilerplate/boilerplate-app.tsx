0d0ca8ec5ad069b37f3ee5636495b79d9780557d
Peter Ondrejka <pondrejk@redhat.com>
2022-04-17 22:42:01 +0200

    typescript boilerplate

---
+++ react_redux_typescript_boilerplate/boilerplate-app.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function App() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col xs={12}>
+          <h1>Hello World</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
