30c65e5a98b7accf8d6e1e0974d45d7bd38be287
Peter Ondrejka <pondrejk@redhat.com>
2022-04-24 11:43:14 +0200

    react 18 update

--- react_redux_typescript_boilerplate/boilerplate-index.tsx
+++ react_redux_typescript_boilerplate/boilerplate-index.tsx
@@ -1,23 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from "./reportWebVitals";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
